86b7cbf3b32603cf98287cca10310cb13b9907e5
Jongerr <jontmichie@gmail.com>
2017-07-10 18:23:59 -0500

    Begun coding core window

---
+++ receiving.py
@@ -0,0 +1,16 @@
+
+import sys
+from PyQt5.QtWidgets import QMainWindow
+
+
+class Receiving(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+        
+
+    def initUI(self):
+
+        
